d3ec76bf385ad955ae8f60e87d43c1452704b9f3
inespierson <ines.pierson@epita.fr>
2022-11-02 22:15:21 +0100

    store in csv expense and users, create a user, choose between existing users, add people involved

--- expense.py
+++ expense.py
@@ -1,4 +1,5 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
@@ -13,18 +14,42 @@ expense_questions = [
     },
     {
         "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
+        "name":"other users involved",
+        "message":"New Expense - Other users involved: ",
     },
 
 ]
 
+def get_users():
+    users = []
+    with open('users.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            users.append(row[0])
+    return users
+
+def choose_user():
+    users = get_users()
+    user_questions = [
+        {
+            "type":"list",
+            "name":"name",
+            "message":"New Expense - Spender: ",
+            "choices": users
+        },
+    ]
+    user = prompt(user_questions)
+    return user['name']
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    infos['spender'] = choose_user()
     print("Expense Added !")
+    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open('expense_report.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([infos['amount'], infos['label'], infos['spender'], infos["other users involved"]])
     return True
 
 
--- user.py
+++ user.py
@@ -1,8 +1,18 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    },
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
+    user = prompt(user_questions)
+    with open('users.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([user['name']])
     return
